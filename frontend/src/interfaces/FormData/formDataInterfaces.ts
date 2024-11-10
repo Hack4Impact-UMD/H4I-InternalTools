509ce1aaaadc12ba66621e53a3536762e08b004e
sgubba1 <sgubba15@gmail.com>
2024-11-10 17:42:45 -0500

    Added in engineer and tech lead forms and data

--- frontend/src/interfaces/FormData/formDataInterfaces.ts
+++ frontend/src/interfaces/FormData/formDataInterfaces.ts
@@ -18,6 +18,30 @@ export interface BootCampData {
   challengePersist: string;
 }
 
+//Questions for the Engineer app
+export interface EngineerData {
+  whyEngineer: string;
+  engineerFit: string;
+  gitExperience: string;
+  codeExperience: string;
+  mistakeExperience: string;
+  sprintExperience: string;
+  technicalSubmission: string;
+  comments: string;
+}
+
+//Questions for the Tech Lead app
+export interface TechLeadData {
+  whyTechLead: string;
+  techLeadFit: string;
+  gitExperience: string;
+  codeExperience: string;
+  projectExperience: string;
+  technicalSubmission: string;
+  comments: string;
+}
+
+
 // All forms will need to import this interface to update the state in the
 // parent.
 export interface FormProps<T extends object> {
