1b83a49bf628950ea1c54bc663e827d9fc841d7c
Kalpana Iyer <kalpana04.iyer@gmail.com>
2024-11-12 23:41:06 -0500

    organize

---
+++ h4i-internal/src/App.tsx
@@ -0,0 +1,15 @@
+// src/App.tsx
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Login from './pages/login'
+
+function App() {
+  return (
+      <div>
+        <h1>Application Portal</h1>
+        <Login></Login>
+      </div>
+  );
+}
+
+export default App;
