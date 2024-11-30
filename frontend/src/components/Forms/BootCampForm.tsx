724e302c8145916985148ac4e5beeb19450d0880
Nadia Meyerovich <nmeyerov@terpmail.umd.edu>
2024-11-30 13:32:33 -0500

    Bootcamp and PM CSS added

--- frontend/src/components/Forms/BootCampForm.tsx
+++ frontend/src/components/Forms/BootCampForm.tsx
@@ -12,35 +12,44 @@ const BootCampForm: React.FC<FormProps<BootCampData>> = ({
   sectionFormData,
 }) => {
   return (
-    <div>
-      <div>
-        <strong>Bootcamp Questions</strong> add description...
+    <div className="form-body">
+      <div className="form-heading">
+        <strong>Bootcamp Questions</strong>
       </div>
 
-      <TextAnswer
-        heading="Tell us why you are interested in joining the bootcamp."
-        subHeading="Please limit your response to a short paragrah (250 words)."
-        value={sectionFormData.interest}
-        onChange={(value) => onFormDataChange("interest", value)}
-        placeholder="Long answer text"
-        required
-      />
+      <div className="form-questions">
+        <p>Bootcamp Responsibilities:</p>
+        <p className="form-responsibilities-list">
+          - responsible for implementation of all technological aspects of product<br />
+          - attend team meetings<br />
+          - complete assigned tasks by the given deadline<br />
+          - update Tech Lead and PM with any relevant issues<br />
+        </p>
+        <TextAnswer
+          heading="Tell us why you are interested in joining the bootcamp."
+          subHeading="Please limit your response to a short paragrah (250 words)."
+          value={sectionFormData.interest}
+          onChange={(value) => onFormDataChange("interest", value)}
+          placeholder="Long answer text"
+          required
+        />
 
-      <TextAnswer
-        heading="Tell us about a time you had to learn a new skill to accomplish a task."
-        value={sectionFormData.newSkill}
-        onChange={(value) => onFormDataChange("newSkill", value)}
-        placeholder="Long answer text"
-        required
-      />
+        <TextAnswer
+          heading="Tell us about a time you had to learn a new skill to accomplish a task."
+          value={sectionFormData.newSkill}
+          onChange={(value) => onFormDataChange("newSkill", value)}
+          placeholder="Long answer text"
+          required
+        />
 
-      <TextAnswer
-        heading="Describe a time when you had to persist through a significant challenge. How did you approach it and what did you learn from that experience?"
-        value={sectionFormData.challengePersist}
-        onChange={(value) => onFormDataChange("challengePersist", value)}
-        placeholder="Long answer text"
-        required
-      />
+        <TextAnswer
+          heading="Describe a time when you had to persist through a significant challenge. How did you approach it and what did you learn from that experience?"
+          value={sectionFormData.challengePersist}
+          onChange={(value) => onFormDataChange("challengePersist", value)}
+          placeholder="Long answer text"
+          required
+        />
+      </div>
     </div>
   );
 };
